a55ba946f823ccca424dc2f12996cb72ed3bc1bb
PEGI <87621661+pablogl2002@users.noreply.github.com>
2023-10-23 23:37:56 +0200

    inicio
    pruebas con la interafaz

---
+++ RMoperations.py
@@ -0,0 +1,19 @@
+
+
+def suc(i):
+    ''' Incrementa en una unidad el contenido del registro Ri 
+            [i] <- [i] + 1
+        sigue la ejecución
+    '''    
+    pass
+
+def pre(i):
+    ''' Si [i] > 0: 
+            decrementa una unidad el contenido del registro Ri
+                [i] <- [i] - 1
+            sigue la ejecución            
+        otro caso:
+            sigue la ejecución
+    '''
+    pass
+
