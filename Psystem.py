b23ba807bb5f4dace1405445acba1586d3b2e440
pablogl2002 <pablogl2002@gmail.com>
2023-11-15 11:18:23 +0100

    struct comienzo

--- Psystem.py
+++ Psystem.py
@@ -2,15 +2,22 @@ import membrane
 
 class PSystem:
 
-    def __init__(self, H=0, membranes:dict={}, rules:dict={}, plasmids=None, i0=None):
-        self.membNum = H
-        self.membranes = membranes
-        self.rules = rules
-        self.plasmids = plasmids
-        self.outRegion = i0
-    
+    def prove_struct(self, struct):
+        open = struct[0]
+        if open == '1':
+            for m in struct[1:]:
+                if m != open[-1]:
+                    open = open + m
+                else:
+                    
+                    
 
 
-    # def add_rule(self, rule, prio=0):
-    #     self.membrane["rules"] = self.membrane["rules"].get(rule, 0) + prio
-    #     if self.min_prio < prio: self.min_prio = prio    
+        
+
+    def __init__(self, V=[], base_struct="11", membobjects=[], i0=None):
+        self.membNum
+        self.membranes
+        self.rules
+        self.plasmids
+        self.outRegion = i0    
