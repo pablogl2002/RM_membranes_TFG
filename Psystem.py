ad2bcc92764f062a229bc3edd10d2886888445e3
pablogl2002 <pablogl2002@gmail.com>
2023-11-16 12:02:32 +0100

    gen_struct
    falta añadir hijos al generar estructura

--- Psystem.py
+++ Psystem.py
@@ -1,23 +1,28 @@
-import membrane
+from membrane import *
 
 class PSystem:
 
-    def prove_struct(self, struct):
-        open = struct[0]
-        if open == '1':
-            for m in struct[1:]:
-                if m != open[-1]:
-                    open = open + m
-                else:
-                    
-                    
-
-
-        
-
     def __init__(self, V=[], base_struct="11", membobjects=[], i0=None):
-        self.membNum
-        self.membranes
-        self.rules
-        self.plasmids
-        self.outRegion = i0    
+        self.membNum = 1
+        self.membranes = {}
+        self.rules = {}
+        self.plasmids = {}
+        self.outRegion = i0
+
+        self.gen_struct(base_struct)
+
+    def gen_struct(self, struct):
+        open = ''
+        for m in struct:
+            if open == '' or m != open[-1]:
+                parent = None if open == '' else open[-1]
+                self.membranes[self.membNum] = self.membranes.get(self.membNum, Membrane(id=self.membNum, parent=parent, objects={}))
+                open = open + m
+                self.membNum = self.membNum + 1
+            else:
+                open = open[:-1]
+
+        if open != '':
+            self.membNum = 1
+            self.membranes = {}
+            print('Incorrect membrane structure')
--- membrane.py
+++ membrane.py
@@ -1,24 +1,22 @@
-
 class Membrane:
 
-    def __init__(self, id, parent, childs, plasmids, rules:dict={}, objects:dict={}):
-        self.membrane = {
-            "id": id,
-            "parent": parent,
-            "childs": childs,
-            "plasmids": plasmids,
-            "rules": rules,
-            "objects": objects
-        }
+    def __init__(self, id:int, parent:int=None, objects:dict={}):
+        self.id = id
+        self.parent = parent
+        self.childs = set()
+        self.rules = set()
+        self.plasmids = set()
+        self.objects = objects
 
-    def get_rules(self):
-        return self.membrane['rules']
+    def add_childs(self, childs:list):
+        for child in childs:
+            self.childs.add(child)
     
-    # def iteration(self):
-    #     act_prio = 0
-    #     minor_prior = set()
-    #     for rule, prio in self.rules.items:
-    #         if prio > act_prio:
-    #             minor_prior.add((rule, prio))
-    #         else:
-    #             pass
+    def add_rules(self, rules:list):
+        for rule in rules:
+            self.rules.add(rule)
+    
+    def add_plasmids(self, plasmids:list):
+        for plasmid in plasmids:
+            self.rules.add(plasmid)
+     
