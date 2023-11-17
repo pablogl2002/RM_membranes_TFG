256b9ba218c5bb64cdd44806ee783ca32e1e51fa
pablogl2002 <pablogl2002@gmail.com>
2023-11-17 01:18:59 +0100

    reglas factibles

--- Psystem.py
+++ Psystem.py
@@ -2,25 +2,25 @@ from membrane import *
 
 class PSystem:
 
-    def __init__(self, V:list=[], base_struct="11", membobjects=[], i0=None):
+    def __init__(self, V:list=[], base_struct="11", m_objects=[], m_rules=[], i0=1):
         self.alphabet = set(V)
         self.membranes = {}
-        self.rules = {}
         self.plasmids = {}
         self.outRegion = i0
 
-        self.gen_struct(base_struct)
+        self.gen_struct(base_struct, m_objects, m_rules)
+        print(self.membranes[1].feasible_rules())
 
-    def gen_struct(self, struct):
+    def gen_struct(self, struct, m_objects, m_rules):
         open = struct[0]
         id = int(open)
-        self.membranes[id] = self.membranes.get(id, Membrane(V=self.alphabet, id=id, parent=None, objects='bbc'))
+        self.membranes[id] = self.membranes.get(id, Membrane(V=self.alphabet, id=id, parent=None, objects=m_objects[id-1], rules=m_rules[id-1]))
 
         for m in struct[1:]:
             if m != open[-1]:
                 self.membranes[int(open[-1])].add_child(id + 1)
                 id = id + 1
-                memb = Membrane(V=self.alphabet, id=id, parent=open[-1], objects='aabc')
+                memb = Membrane(V=self.alphabet, id=id, parent=open[-1], objects=m_objects[id-1], rules=m_rules[id-1])
                 self.membranes[id] = self.membranes.get(id, memb)
                 open = open + m
             else:
@@ -29,3 +29,11 @@ class PSystem:
         if open != '':
             self.membranes = {}
             print('Incorrect membrane structure')
+
+    
+# from PSystem import *
+# ps = PSystem(V=['a','b','c'], base_struct='1221', m_objects=['aaaaac','ab'], m_rules=[[('aa','b'),('c','a')],[('c','ca')]])
+
+ps = PSystem(V=['a','b','c'], base_struct='1221', m_objects=['aa',''], m_rules=[[('a','ab2c2c2'),('aa','a0a0')],[]], i0=2)
+# for c in 'a2':
+#     print(c.isdigit())
--- membrane.py
+++ membrane.py
@@ -1,14 +1,16 @@
 class Membrane:
 
-    def __init__(self, V:list, id:int, parent:int=None, objects:str=''):
+    def __init__(self, V, id:int, parent:int=None, objects:str='', rules:list=[]):
         self.alphabet = V
         self.id = id
         self.parent = parent
         self.childs = set()
-        self.rules = set()
+        self.rules_id = 0
+        self.rules = {}
         self.plasmids = set()
         self.objects = {}
         self.add_objects(objects)
+        self.add_rules(rules)
 
     def add_child(self, child:int):
         self.childs.add(child)
@@ -19,15 +21,14 @@ class Membrane:
     
     def add_rules(self, rules:list):
         for rule in rules:
-            self.rules.add(rule)
+            self.rules_id = self.rules_id + 1
+            self.rules[self.rules_id] = self.rules.get(self.rules_id, rule)
     
     def add_plasmids(self, plasmids:list):
         for plasmid in plasmids:
             self.rules.add(plasmid)
     
     def add_objects(self, objects:str):
-        #suma = sum([objects.count(v) for v in self.V])
-        #if suma != len(objects):
         suma = 0
         prev_objs = self.objects
         for obj in self.alphabet:
@@ -37,3 +38,34 @@ class Membrane:
         if suma != len(objects):
             self.objects = prev_objs
             print(f'Objects given not in alphabet({self.alphabet})')
+
+    def feasible_rules(self):
+        feasible_r = set()
+        for rule in self.rules.keys():
+            #print(self.rules[rule][0])
+            aux = True
+            for obj in self.alphabet:
+                #print(f'self.objects[{obj}] | self.rules[{rule}].count({obj}) ',self.objects[obj], self.rules[rule][0].count(obj))
+                if self.objects[obj] < self.rules[rule][0].count(obj):
+                    aux = False
+                    break
+            if aux:
+                print(self.rules[rule][1])
+                for obj in self.rules[rule][1]:
+                    if (not(obj.isdigit()) and obj not in self.alphabet) or (obj.isdigit() and int(obj) not in self.childs and int(obj) != 0):
+                        aux = False
+                        break
+            if aux: feasible_r.add(rule)
+        return feasible_r
+    
+    # def feasible_rules2(self):
+    #     feasible_r = set()
+    #     for rule in self.rules.keys():
+    #         #print(self.rules[rule][0])
+    #         aux = True
+    #         for obj in self.alphabet:
+    #             #print(f'self.objects[{obj}] | self.rules[{rule}].count({obj}) ',self.objects[obj], self.rules[rule][0].count(obj))
+    #             if self.objects[obj] < self.rules[rule][0].count(obj):
+    #                 aux = False
+    #         if aux: feasible_r.add(rule)
+    #     return feasible_r
