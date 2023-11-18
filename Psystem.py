3bd67ad02d44c9a8fc815d91d6d2e2c0c7446f3b
pablogl2002 <pablogl2002@gmail.com>
2023-11-18 01:20:13 +0100

    Sistema P funcional
    sin relaciones entre reglas y sin plásmidos

--- Psystem.py
+++ Psystem.py
@@ -9,18 +9,17 @@ class PSystem:
         self.outRegion = i0
 
         self.gen_struct(base_struct, m_objects, m_rules)
-        # for memb in self.membranes.keys():
-        #     print(self.membranes[memb].feasible_rules())
-        
-        #print(self.membranes[2].feasible_rules())
 
-        while(self.membranes[i0].empty):
-            self.evolve_iteration()
+        feasible_rules = self.get_feasible_rules()
+        while(feasible_rules != []):
+            self.evolve(feasible_rules)
             print("--------------------------------------------------------------------------------------------")
-            print(self.membranes)
-            for id, memb in self.membranes.items():
-               print(f'membrana : {id} con objetos: {memb.objects}')
-            
+            print(self.struct_system())
+            feasible_rules = self.get_feasible_rules()
+
+        print("================================================================================================")
+        print(self.membranes[i0].objects)
+
 
     def gen_struct(self, struct, m_objects, m_rules):
         open = struct[0]
@@ -31,7 +30,7 @@ class PSystem:
             if m != open[-1]:
                 self.membranes[int(open[-1])].add_child(id + 1)
                 id = id + 1
-                memb = Membrane(V=self.alphabet, id=id, parent=open[-1], objects=m_objects[id-1], rules=m_rules[id-1])
+                memb = Membrane(V=self.alphabet, id=id, parent=int(open[-1]), objects=m_objects[id-1], rules=m_rules[id-1])
                 self.membranes[id] = self.membranes.get(id, memb)
                 open = open + m
             else:
@@ -40,15 +39,17 @@ class PSystem:
         if open != '':
             self.membranes = {}
             print('Incorrect membrane structure')
-    
-    def evolve_iteration(self):
+
+    def get_feasible_rules(self):
         feasible_rules = []
         for id, memb in self.membranes.items():
             aux = memb.feasible_rules()
-            if len(aux) != 0: feasible_rules.append((id, aux)) 
+            if len(aux) != 0: feasible_rules.append((id, aux))
+        return feasible_rules
 
+    def evolve(self, feasible_rules):
         memb_id, f_rules = random.choice(feasible_rules)
-        rule_id = random.choice(f_rules)
+        rule_id = random.choice(list(f_rules))
         
         lhs, rhs = self.membranes[memb_id].rules[rule_id]
 
@@ -56,31 +57,42 @@ class PSystem:
             self.membranes[memb_id].objects[obj] = self.membranes[memb_id].objects[obj] - 1
         
         parent_id = self.membranes[memb_id].parent
-        for i in enumerate(rhs):
-            if not(i.isdigit()):
-                if rhs[i] == '.':   # disolver membrana
+   
+        for i, _ in enumerate(rhs):
+            if not(rhs[i].isdigit()):
+                if rhs[i] == '.':   # disolver
                     if parent_id != None:
-                        for obj in self.alphabet():
+                        for obj in self.alphabet:
                             value = self.membranes[memb_id].objects[obj]
                             self.membranes[parent_id].objects[obj] = self.membranes[parent_id].objects.get(obj, 0) + value
+                    self.membranes[parent_id].childs.remove(memb_id)
                     self.membranes.pop(memb_id)
-                elif rhs[i] != rhs[-1] and rhs[i + 1].isdigit():  # objecto in (id membrana) o out (0)
-                    self.membranes[memb_id].objects[obj] = self.membranes[memb_id].objects[obj] - 1
-                    if rhs[i+1] == '0': # object out membrane
-                        self.membranes[parent_id].objects[obj] = self.membranes[parent_id].objects[obj] + 1
-                    elif rhs[i+1] in self.membranes[memb_id].childs:
-                        self.membranes[rhs[i+1]].objects[obj] = self.membranes[rhs[i+1]].objects[obj] + 1
-                else:   # adicion de objetos
-                    self.membranes[memb_id].objects[obj] = self.membranes[memb_id].objects.get(rhs[i], 0) + 1
-    
+                elif i+1 != len(rhs) and rhs[i+1].isdigit():  # in to child | out to parent    
+                    id = int(rhs[i+1])
+                    if id in self.membranes[memb_id].childs:
+                        self.membranes[id].objects[rhs[i]] = self.membranes[id].objects[rhs[i]] + 1
+                    elif id == 0:
+                        if parent_id != None:
+                            print('id == 0')
+                            print(self.membranes[parent_id])
+                            self.membranes[parent_id].objects[rhs[i]] = self.membranes[parent_id].objects[rhs[i]] + 1
+                else:
+                    self.membranes[memb_id].objects[rhs[i]] = self.membranes[memb_id].objects[rhs[i]] + 1
 
-    def print_system(self):
-        for memb in self.membranes:
-            pass
+    def struct_system(self, struct='', id=1):
+        struct = f'[{id}{self.membranes[id].objects}'
+        if self.membranes[id].childs != {}:
+            for id_child in self.membranes[id].childs:
+                struct += self.struct_system(struct, id_child)
+        struct += f']{id}'
+        return struct
+
 
 # from PSystem import *
 # ps = PSystem(V=['a','b','c'], base_struct='1221', m_objects=['aaaaac','ab'], m_rules=[[('aa','b'),('c','a')],[('c','ca')]])
+# ps = PSystem(V=['a','b','c'], base_struct='1221', m_objects=['a',''], m_rules=[[('a','b2')],[]])
+# ps = PSystem(V=['a','b','c'], base_struct='1221', m_objects=['','a'], m_rules=[[('a','b2')],[('a','c0')]])
+# ps = PSystem(V=['a','b','c'], base_struct='1221', m_objects=['','b'], m_rules=[[],[('b','a'), ('a','c.')]])
+ps = PSystem(V=['a','b','c'], base_struct='1221', m_objects=['aa',''], m_rules=[[('a','ab2c2c2'),('aa','a0a0')],[]], i0=2)
 
-ps = PSystem(V=['a','b','c'], base_struct='1221', m_objects=['aa',''], m_rules=[[('a','ab2c2c2'),('aa','a0a0')],[('c','.')]], i0=2)
-# for c in 'a2':
-#     print(c.isdigit())
+# ps = PSystem(V=['a','b','c'], base_struct='1221', m_objects=['aa',''], m_rules=[[('a','ab2c2c2'),('aa','a0a0')],[]])
--- membrane.py
+++ membrane.py
@@ -4,7 +4,6 @@ class Membrane:
 
     def __init__(self, V, id:int, parent:int=None, objects:str='', rules:list=[]):
         self.alphabet = V
-        print(self.alphabet)
         self.id = id
         self.parent = parent
         self.childs = set()
@@ -12,7 +11,6 @@ class Membrane:
         self.rules = {}
         self.plasmids = set()
         self.objects = {}
-        self.empty = True
         self.rhs_alphabet = V.copy()
 
         self.rhs_alphabet.add('0')
@@ -20,6 +18,7 @@ class Membrane:
 
         self.add_objects(objects)
         self.add_rules(rules)
+
     # para eliminar entradas de plasmidos en rules añadir set ids_reglas plasmidos y con rules.pop(id) o rules.del(id) se elimina la entrada al dict
 
     def add_child(self, child:int):
@@ -38,48 +37,29 @@ class Membrane:
     def add_objects(self, objects:str):
         if len(objects) != 0:
             self.empty = False
-            suma = 0
-            prev_objs = self.objects
-            for obj in self.alphabet:
-                count = objects.count(obj)
-                self.objects[obj] = self.objects.get(obj, 0) + count
-                suma = suma + count
-            if suma != len(objects):
-                self.objects = prev_objs
-                print(f'Objects given not in alphabet({self.alphabet})')
+        suma = 0
+        prev_objs = self.objects
+        for obj in self.alphabet:
+            count = objects.count(obj)
+            self.objects[obj] = self.objects.get(obj, 0) + count
+            suma = suma + count
+        if suma != len(objects):
+            self.objects = prev_objs
+            print(f'Objects given not in alphabet({self.alphabet})')
 
     def feasible_rules(self):
         feasible_r = set()
         for rule in self.rules.keys():
             aux = True
             for obj in self.alphabet:
-                print(obj)
                 if self.objects[obj] < self.rules[rule][0].count(obj):
                     aux = False
                     break
             if aux:
-                print(self.rules[rule][1])
                 for obj in self.rules[rule][1]:
-#                    if (not(obj.isdigit()) and obj not in self.alphabet and obj != '.') or (obj.isdigit() and int(obj) not in self.childs and int(obj) != 0):
                     if obj not in self.rhs_alphabet:
                         aux = False
                         break
             if aux: feasible_r.add(rule)
         return feasible_r
-
-    # def apply_rule(self, rules:list):
-    #     id = random.choice(rules)
-    #     lhs, rhs = self.rules[id]
-        
-    #     for obj in lhs:
-    #         self.objects[obj] = self.objects[obj] - 1
-        
-    #     for i in enumerate(rhs):
-    #         if rhs[i] == '.':   # disolver membrana
-    #             # return 'dissolve', self.id 
-    #             pass
-    #         elif rhs[i] != rhs[-1] and rhs[i + 1].isdigit():  # objecto in (id membrana) o out (0)
-    #             pass
-    #         else:   # adicion de objetos
-    #             self.objects[rhs[i]] = self.objects.get(rhs[i], 0) + 1
         
